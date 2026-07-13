29af19169e527d24abab90cbf511c04f7d04eef9
gepam-science-hub <gepam2025@gmail.com>
2026-07-13 15:23:33 +0300

    Add pastPapers data structure for multiple forms

---
+++ pastpapers-data.js
@@ -0,0 +1,38 @@
+const pastPapers = {
+
+    form1: {
+        physics: [],
+        chemistry: []
+    },
+
+
+    form2: {
+        physics: [],
+        chemistry: []
+    },
+
+
+    form3: {
+        physics: [],
+        chemistry: []
+    },
+
+
+    form4: {
+        physics: [],
+        chemistry: []
+    },
+
+
+    form5: {
+        physics: [],
+        chemistry: []
+    },
+
+
+    form6: {
+        physics: [],
+        chemistry: []
+    }
+
+};
